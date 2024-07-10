915d715a14f71b6a7bb340a4c4e72f91fbc3b3d7
sajeewa0825 <sajeewa0825@gmail.com>
2024-07-10 14:52:01 +0530

    add env file for html

--- module/user/controlers/adminRegForm.js
+++ module/user/controlers/adminRegForm.js
@@ -1,9 +1,18 @@
 const path = require('path');
-const jwt = require('jsonwebtoken');
+const fs = require('fs');
+require('dotenv').config();
 
 const adminRegForm = async (req, res) => {
     try {
-        res.sendFile(path.join(__dirname, 'public', 'adminReg.html'));
+        const htmlFilePath = path.join(__dirname, 'public', 'adminReg.html');
+        fs.readFile(htmlFilePath, 'utf8', (err, data) => {
+            if (err) {
+                return res.status(500).send('Error reading the HTML file');
+            }
+            // Replace the placeholder with the environment variable value
+            const updatedHtml = data.replace('{{BASE_URL}}', process.env.BASE_URL);
+            res.send(updatedHtml);
+        });
     } catch (error) {
         res.status(400).send('Something went wrong!');
     }
--- module/user/controlers/resetEmailVerify.js
+++ module/user/controlers/resetEmailVerify.js
@@ -1,27 +1,35 @@
 const path = require('path');
-const mongoose = require('mongoose');
+const fs = require('fs');
+require('dotenv').config();
 const jwt = require('jsonwebtoken');
-const db = require("../../../model/mysql/index")
+const db = require("../../../model/mysql/index");
 
 // Mysql DB model call
 const UserDB = db.user;
 
 const verifyEmail = async (req, res) => {
     const { token } = req.query;
-    //const userModel = mongoose.model('user');
 
     try {
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
         // Check if the user exists
-        //const user = await userModel.findOne({ email: decoded.data });
         const user = await UserDB.findOne({ where: { email: decoded.data } });
         if (!user) {
             return res.status(400).send('Invalid Email.');
         }
 
-        // If the user is found, send the HTML file
-        res.sendFile(path.join(__dirname, 'public', 'passwordReset.html'));
+        // Read the HTML file
+        const htmlFilePath = path.join(__dirname, 'public', 'passwordReset.html');
+        fs.readFile(htmlFilePath, 'utf8', (err, data) => {
+            if (err) {
+                return res.status(500).send('Error reading the HTML file');
+            }
+
+            // Replace the placeholder with the environment variable value
+            const updatedHtml = data.replace('{{BASE_URL}}', process.env.BASE_URL);
+            res.send(updatedHtml);
+        });
     } catch (error) {
         res.status(400).send('Invalid or expired token.');
     }
