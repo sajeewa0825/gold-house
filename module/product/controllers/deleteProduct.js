c5681fa7aade872fa9a7ff246cc26c40fe78ca82
sajeewa0825 <sajeewa0825@gmail.com>
2024-08-07 02:09:40 +0530

    update delete product

--- module/product/controllers/deleteProduct.js
+++ module/product/controllers/deleteProduct.js
@@ -39,6 +39,7 @@ const fs = require('fs');
 const path = require('path');
 const db = require("../../../model/mysql/index");
 const Product = db.products;
+const cloudinary = require("../../../manager/cloudnary");
 
 const deleteProduct = async (req, res) => {
     const productId = req.params.id; // Assuming the product ID is passed as a route parameter
@@ -52,20 +53,38 @@ const deleteProduct = async (req, res) => {
         }
 
         // Parse the images field to get image paths
-        const images = JSON.parse(product.images);
+        // const images = JSON.parse(product.images);
+
+        // Function to extract public_id from secure_url
+        const getPublicIdFromUrl = (url) => {
+            const parts = url.split('/');
+            const fileName = parts.pop();
+            const publicId = parts.slice(7).join('/') + fileName.split('.')[0];
+            //console.log('publicId:', publicId);
+            return publicId;
+        };
+
+        // Delete existing images from Cloudinary
+        if (product.images) {
+            const existingImages = JSON.parse(product.images);
+            await Promise.all(existingImages.map(image => {
+                const publicId = getPublicIdFromUrl(image.url);
+                return cloudinary.uploader.destroy(publicId);
+            }));
+        }
 
         // Delete the product
         await product.destroy();
 
-        // Delete the image files from the filesystem
-        images.forEach(image => {
-            const filePath = path.join(__dirname, '../../../', image.url);
-            fs.unlink(filePath, err => {
-                if (err) {
-                    console.error(`Failed to delete image file: ${filePath}`, err);
-                }
-            });
-        });
+        // // Delete the image files from the filesystem
+        // images.forEach(image => {
+        //     const filePath = path.join(__dirname, '../../../', image.url);
+        //     fs.unlink(filePath, err => {
+        //         if (err) {
+        //             console.error(`Failed to delete image file: ${filePath}`, err);
+        //         }
+        //     });
+        // });
 
 
         res.status(200).json({ message: 'Product and associated images deleted successfully' });
